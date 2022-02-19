92e674c5d4b22c03873728434c9a600a5d20564a
andrea <patini929@gmail.com>
2022-02-19 14:06:48 +0100

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Andrea on 19/02/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
